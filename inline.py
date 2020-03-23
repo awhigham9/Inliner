1e751db7d6e065938b31de71c0c179052f4d1038
awhigham9 <awhigham9@gmail.com>
2020-03-23 01:52:09 -0400

    Added command line interface script, formerly runner.py, now renamed inline.py

---
+++ inline.py
@@ -0,0 +1,26 @@
+import json
+from inline import *
+import argparse
+
+def run(config_path, input_path, output_path, top_modules=None):
+    print("Running. . .")
+    i = Inliner(config_path,input_path)
+    i.inline()
+    with open("out.v",'w') as f:
+        if top_modules:
+            for name in top_modules:
+                f.write("".join(list(map(lambda x : x.to_string(), i._inlined_modules[name].header))))
+                f.write("".join(list(map(lambda x : x.to_string(), i._inlined_modules[name].body))))
+        else:
+            for name in i._inlined_modules:
+                f.write("".join(list(map(lambda x : x.to_string(), i._inlined_modules[name].header))))
+                f.write("".join(list(map(lambda x : x.to_string(), i._inlined_modules[name].body))))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Performs the inlining process on file using the settings specified in config")
+    parser.add_argument("file", help="the input file whose modules will be inlined")
+    parser.add_argument("-c", "--config", nargs=1, help="the configuration file that speficies the verilog keywords and special characters (default: %(default)s)", default="config.json")
+    parser.add_argument("-t", "--top", nargs="?", action="append", const="", help="the top level modules that should appear in the output path; if not specified, all modules are dumped to the output path")
+    parser.add_argument("-o", "--out", nargs=1, default="out.v", help="the output file path to which the inlined files will be dumped(default: %(default)s)")
+    args = parser.parse_args()
+    run(args.config[0], args.file, args.out[0], args.top)
